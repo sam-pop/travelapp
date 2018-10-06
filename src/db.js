703910b23b3a263cf8567e0a4d08990ca4d1b1fe
Lior Sabag <lior@kanghealth.io>
2018-10-06 16:47:14 +0300

    get data from db

--- src/db.js
+++ src/db.js
@@ -4,16 +4,19 @@ import {
   AnonymousCredential
 } from 'mongodb-stitch-browser-sdk'
 
+import { ObjectId } from 'bson'
+
 const client = Stitch.initializeDefaultAppClient('travelapp-ewvje')
 
 const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('dev')
 
-const getClient = () => client.auth.loginWithCredential(new AnonymousCredential())
+const anonymousCredential = new AnonymousCredential()
+
+const getClient = () => client.auth.loginWithCredential(anonymousCredential)
   .then(() => db)
 
 getClient().then(db =>
   Promise.all([
-    db.collection('trips').updateOne({ owner_id: client.auth.user.id }, { $set: { number: 42 } }, { upsert: true }),
     db.collection('trips').find({ owner_id: client.auth.user.id }, { limit: 100 }).asArray()
   ])
 ).then(([nothing, docs]) => {
@@ -23,6 +26,4 @@ getClient().then(db =>
   console.error(err)
 })
 
-export default {
-  getClient
-}
+export { getClient, ObjectId }
